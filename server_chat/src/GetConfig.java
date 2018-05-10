1136e284b3e8f34c82b781eeabf4bc5c46ffbdd1
loay naser <looaay93@hotmail.com>
2018-05-11 01:42:52 +0300

    commit 1

---
+++ server_chat/src/GetConfig.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GetConfig {
+
+
+    protected static String getConfig(String Choose) {
+        String result = null;
+        Properties prop = new Properties();
+        InputStream input = null;
+        try {
+            if (new File( "config.properties" ).exists()) {
+                input = new FileInputStream( "config.properties" );
+                // load a properties file
+                prop.load( input );
+                // get the property value and print it out
+                result = String.valueOf( prop.getProperty( Choose ) );
+            }
+            //return result;
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+}
